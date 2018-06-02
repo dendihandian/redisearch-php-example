7666b286e09aea2734c24b27c2a36f293e5229be
Dendi Handian <dendihandian@gmail.com>
2018-06-03 06:13:03 +0700

    group the routes

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,11 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('products/suggestion', 'ProductsController@suggestion');
-$router->get('products', 'ProductsController@index');
-$router->post('products', 'ProductsController@store');
-$router->get('products/{id}', 'ProductsController@show');
-$router->patch('products/{id}', 'ProductsController@update');
-$router->delete('products/{id}', 'ProductsController@destroy');
+$router->group(['prefix' => 'products'], function ($router) {
+  $router->get('suggestion', 'ProductsController@suggestion');
+  $router->get('/', 'ProductsController@index');
+  $router->post('/', 'ProductsController@store');
+  $router->get('/{id}', 'ProductsController@show');
+  $router->patch('/{id}', 'ProductsController@update');
+  $router->delete('/{id}', 'ProductsController@destroy');
+});
